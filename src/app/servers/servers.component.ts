a5e9b0892c49d417042dc39d2bc1d110dd468b2d
rajvin-cn <rajvin.k2020@gmail.com>
2024-02-18 18:55:15 -0330

    Datbinding  Examples

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
+export class ServersComponent implements OnInit {
+  ngOnInit(): void {
+   console.log("Init called");
+    // throw new Error('Method not implemented.');
+  }
+
+  allowNewServer: boolean = false;
+  serverCreationStatus:string = " No Servers Created!!";
+  serverName:string ="Not set";
+
+  constructor()
+  {
+    setTimeout(() =>this.allowNewServer = true, 2000);
+  }
+ 
+  onAddNewServer()
+  {
+    this.serverCreationStatus ="New Servers are getting Added , The Name of the Server is " + this.serverName;
+
+   
+}
+onUpdateServerName(event: Event) 
+{
+  this.serverName = (<HTMLInputElement>event.target).value;
+}
 
 }
